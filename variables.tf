e3cf29df18925cc3d52301070b614d7dbdc10294
drrubinai <drrubinai@gmail.com>
2020-09-18 15:28:47 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,3 +24,9 @@ variable "server_port" {
   type        = number
   default     = 8080
 }
+
+variable "additional_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8081
+}
